012c88836bff14b9d85f5ffb7178779e37b6b1c4
taavirehemagi <taavirehemagi@gmail.com>
2016-04-09 13:54:39 +0300

    include config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+//Configuration for running tests and examples against the Twilio API
+module.exports = {
+    accountSid:'ACaf09d922cad2ddd30e229f7acc5bd3c2',
+    authToken:'812cf17ab1048a4093477046a44a09c2',
+    from:'+3728803833', //The Twilio number you've bought or configured
+    to:'+37256615540' //The number you would like to send messages to for testing
+};
