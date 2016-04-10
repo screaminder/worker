2adee7c2613141f18c5b9400b62871c4e8738b09
taavirehemagi <taavirehemagi@gmail.com>
2016-04-10 12:59:24 +0300

    add debugging.

--- actions/twilio_callback.js
+++ actions/twilio_callback.js
@@ -4,6 +4,7 @@ const twilioCallback = () =>  {
 	
 	function handleCall(request, response) {
 		console.log("handling")
+		console.log(request.body)
 		console.log(request.body.From)
 		console.log(request.body.CallStatus);
 	};
